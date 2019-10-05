b8fae31e067a3bc89f172981d94e0fc4521a2a4b
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-10-05 17:41:56 +0200

    firebase config added

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const config = {
+	apiKey: "AIzaSyCQutkAOwqPtXQrkipJxrA52cd7OQXAfgM",
+	authDomain: "vue-personal-organizer.firebaseapp.com",
+	databaseURL: "https://vue-personal-organizer.firebaseio.com",
+	projectId: "vue-personal-organizer",
+	storageBucket: "vue-personal-organizer.appspot.com",
+	messagingSenderId: "93677670654",
+	appId: "1:93677670654:web:30880d31c14fbfb79c15af",
+	measurementId: "G-3L6WEK86GX"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+export default firebaseApp.firestore();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
-  <HelloWorld />
+  <div class="about">
+    <h1>This is a home page</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
