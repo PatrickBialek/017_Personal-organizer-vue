98b5877c8d4d9a5ca0ae50ee12f15ac4b9c2bbc2
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-10-21 20:49:18 +0200

    Preprare routes organization

--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import SignIn from "./views/SignIn.vue";
+import SignUp from "./views/SignUp.vue";
 import Error404 from "./views/Error404.vue";
+import Notes from "./views/Notes/index.vue";
 
 Vue.use(Router);
 
@@ -14,10 +17,19 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      component: () =>
-        import( /* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/sign-in",
+      name: "SignIn",
+      component: SignIn
+    },
+    {
+      path: "/sign-up",
+      name: "SignUp",
+      component: SignUp
+    },
+    {
+      path: "/notes",
+      name: "Notes",
+      component: Notes
     },
     {
       path: "*",
