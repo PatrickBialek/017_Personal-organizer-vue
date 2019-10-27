1e8c9a664808df42439b284fe25b8310c28258b2
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-10-27 19:58:03 +0100

    working on sign up

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,14 +2,25 @@
   <v-container fill-height grid-list-md>
     <v-layout row wrap align-center class="justify-center">
       <v-card class="my-3" min-width="300">
+        <v-row class="ma-4">
+          <v-btn class="mb-4" width="100%">Continue with google</v-btn>
+        </v-row>
+        <v-row class="mx-4 mt-4 justify-center align-center">
+          <v-divider></v-divider>
+          <span class="mx-4">OR</span>
+          <v-divider></v-divider>
+        </v-row>
         <v-card-title>
-          <h2 class="title grey--text text--darken-2" min-width="300">Sing In</h2>
+          <h2 class="title text--grey text--darken-2" min-width="300">Sing In</h2>
         </v-card-title>
         <v-form class="pa-4">
           <v-text-field label="Email:" v-model="user.email"></v-text-field>
           <v-text-field label="Passowrd" :type="'password'" v-model="user.password"></v-text-field>
           <v-btn flat class="primary">Sing In</v-btn>
         </v-form>
+        <v-row class="mx-4 mb-4">
+          <router-link class="link" to="/sign-up">I don't have an account yet.</router-link>
+        </v-row>
       </v-card>
     </v-layout>
   </v-container>
@@ -28,3 +39,9 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="scss">
+.link {
+  text-decoration: none;
+}
+</style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,3 +1,51 @@
 <template>
-  <div>sign up</div>
+  <v-container fill-height grid-list-md>
+    <v-layout row wrap align-center class="justify-center">
+      <v-card class="ma-3" min-width="300">
+        <v-row class="ma-4">
+          <v-btn class="mb-4" width="100%">Continue with google</v-btn>
+        </v-row>
+        <v-row class="mx-4 mt-4 justify-center align-center">
+          <v-divider></v-divider>
+          <span class="mx-4">OR</span>
+          <v-divider></v-divider>
+        </v-row>
+        <v-card-title>
+          <h2 class="title text--grey text--darken-2">Sing Up</h2>
+        </v-card-title>
+        <v-form class="pa-4">
+          <v-text-field label="Email:" v-model="user.email"></v-text-field>
+          <v-text-field label="Name" v-model="user.name"></v-text-field>
+          <v-text-field label="Password" :type="'password'" v-model="user.password"></v-text-field>
+          <v-text-field label="Password" :type="'password'" v-model="user.passwordRepeated"></v-text-field>
+          <v-btn flat class="primary">Sign Up</v-btn>
+        </v-form>
+        <v-row class="mx-4 mb-4">
+          <router-link class="link" to="/sign-in">I have and account.</router-link>
+        </v-row>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      user: {
+        email: "",
+        name: "",
+        password: "",
+        passwordRepeated: ""
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.link {
+  text-decoration: none;
+}
+</style>
