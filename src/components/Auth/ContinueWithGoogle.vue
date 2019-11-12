525a15d3bb48e2f42002e377f2266093168f977b
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-11-12 17:46:26 +0100

    <ContinueWithGoole.vue> component has been created

---
+++ src/components/Auth/ContinueWithGoogle.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row class="ma-4">
+    <v-btn
+      class="red darken-2 white--text mb-4"
+      @click="continueWithGoogle"
+      width="100%"
+      :loading="loading"
+    >Continue with google</v-btn>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false
+    };
+  },
+  methods: {
+    continueWithGoogle() {
+      this.loading = true;
+      const provider = new firebase.auth.GoogleAuthProvider(),
+        errors = [];
+
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(res => {
+          this.createUserDatabase();
+          this.loading = false;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.loading = false;
+        });
+    },
+    createUserDatabase() {
+      const user = firebase.auth().currentUser,
+        email = user.email,
+        name = user.displayName;
+
+      let ref = db.collection("users").doc(email);
+      ref.set({
+        name: name
+      });
+    }
+  }
+};
+</script>
