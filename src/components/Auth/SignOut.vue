d3524ee945fb17bb3127e36181fbbb397be1b012
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-11-17 12:18:18 +0100

    Sign out btn added

---
+++ src/components/Auth/SignOut.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-btn @click="signOut" class="grey--text" text small>Sign Out</v-btn>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "SignOut",
+  data() {
+    return {};
+  },
+  methods: {
+    signOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push("/sign-in");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,14 +4,20 @@
       <h1 class="grey--text text--lighten-1 title">Personal Organizer</h1>
     </v-toolbar-title>
     <v-spacer></v-spacer>
+    <signOut />
   </v-toolbar>
 </template>
 
 <script>
+import signOut from "@/components/Auth/SignOut.vue";
+
 export default {
   name: "Header",
   data() {
     return {};
+  },
+  components: {
+    signOut
   }
 };
 </script>
