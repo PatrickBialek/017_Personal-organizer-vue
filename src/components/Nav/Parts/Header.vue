6dfcc8d34349237a2cb643c8f66fc2e3a5d517e3
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-11-21 21:47:33 +0100

    menu finished

--- src/components/Nav/Parts/Header.vue
+++ src/components/Nav/Parts/Header.vue
@@ -7,13 +7,10 @@
       <h1 class="grey--text text--lighten-1 title">Personal Organizer</h1>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <signOut v-if="user"></signOut>
   </v-app-bar>
 </template>
 
 <script>
-import signOut from "@/components/Auth/SignOut.vue";
-
 export default {
   name: "Header",
   data() {
@@ -23,9 +20,6 @@ export default {
     user() {
       return this.$store.getters.getIsUserOnline;
     }
-  },
-  components: {
-    signOut
   },
   methods: {
     drawerHandler() {
--- src/components/Nav/Parts/NavigationDrawer.vue
+++ src/components/Nav/Parts/NavigationDrawer.vue
@@ -1,7 +1,25 @@
 <template>
   <nav>
     <v-navigation-drawer v-model="navigation" app temporary>
-      <v-layout class="pa-4">
+      <v-layout column class="pa-4 fill-height">
+        <v-row class="px-3">
+          <h2>User Name</h2>
+        </v-row>
+        <v-row class="fill-height">
+          <v-list>
+            <v-list-item-group>
+              <v-list-item v-for="(item, key) in items" :key="key">
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-row>
+        <v-spacer></v-spacer>
         <signOut />
       </v-layout>
     </v-navigation-drawer>
@@ -14,7 +32,17 @@ import signOut from "@/components/Auth/SignOut.vue";
 export default {
   name: "NavigationDrawer",
   data() {
-    return {};
+    return {
+      items: [
+        { icon: "dashboard", text: "Dashboard", route: "" },
+        { icon: "view_list", text: "Notes", route: "" },
+        { icon: "watch_later", text: "Time Tracker", route: "" },
+        { icon: "account_balance_wallet", text: "Budget", route: "" },
+        { icon: "wb_sunny", text: "Weather", route: "" },
+        { icon: "trending_up", text: "Exchange Rates", route: "" },
+        { icon: "watch_later", text: "Calendar", route: "" }
+      ]
+    };
   },
   components: {
     signOut
